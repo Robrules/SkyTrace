c89ac876556678789ede465de994863d5b1f4a6b
Robert Bannayan <bannayanr@outlook.com.au>
2026-02-10 23:43:23 +1100

    Add initial Satellite domain model and API endpoint

---
+++ SkyTrace.Api/Controllers/SatellitesController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SkyTrace.Api.Domain;
+
+namespace SkyTrace.Api.Controllers;
+
+[ApiController]
+[Route("api/satellites")]
+public class SatellitesController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        var satellites = new[]
+        {
+            new Satellite
+            {
+                Id = Guid.NewGuid(),
+                Name = "ISS",
+                Latitude = -33.86,
+                Longitude = 151.21,
+                AltitudeKm = 420,
+                Timestamp = DateTimeOffset.UtcNow
+            }
+        };
+
+        return Ok(satellites);
+        
+    }
+}
---
+++ SkyTrace.Api/Domain/Satellite.cs
@@ -0,0 +1,16 @@
+﻿namespace SkyTrace.Api.Domain
+{
+    public class Satellite
+    {
+        public Guid Id  { get; init; }
+        public string Name { get; set; } = string.Empty;
+
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        
+        public double AltitudeKm { get; set; }
+
+        public DateTimeOffset Timestamp  { get; set; }
+
+    }
+}
