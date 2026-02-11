920a33cd2a42ca576f2f01f78ab0b44c3948a466
Robert Bannayan <bannayanr@outlook.com.au>
2026-02-11 23:23:52 +1100

    Add service interface for CelesTrak Api

---
+++ SkyTrace.Api/Services/CelesTraKService.cs
@@ -0,0 +1,12 @@
+﻿using SkyTrace.Api.Domain;
+
+namespace SkyTrace.Api.Services
+{
+    public class CelesTraKService : ICelesTrakService
+    {
+        public Task<IReadOnlyList<Satellite>> GetActiveSatellitesAsync(CancellationToken ct = default)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SkyTrace.Api/Services/ICelesTrakService.cs
@@ -0,0 +1,10 @@
+﻿using SkyTrace.Api.Domain;
+
+namespace SkyTrace.Api.Services
+{
+    public interface ICelesTrakService
+    {
+        Task<IReadOnlyList<Satellite>> GetActiveSatellitesAsync(CancellationToken ct = default);
+
+    }
+}
